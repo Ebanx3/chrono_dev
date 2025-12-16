d1be161cf31eda1c9e31ccd11d093df993d04d84
k3xxv1n <kevincalisto001@gmail.com>
2025-12-16 02:15:22 +0100

    Finish PostReplies

--- backend/src/services/router.ts
+++ backend/src/services/router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import UserRouter from "../entities/user/routes";
 import PostRouter from "../entities/post/routes";
+import PostRepliesRouter from "../entities/post_replies/routes";
 
 const router = Router();
 
 router.use('/user', UserRouter)
 router.use('/post', PostRouter)
+router.use('/post-replies', PostRepliesRouter)
 
 export default router;
